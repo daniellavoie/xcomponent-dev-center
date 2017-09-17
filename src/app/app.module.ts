d7b939095ceea07847ee963faf7e1fca0fb4c6ab
Daniel Lavoie <dlavoie@pivotal.io>
2017-09-17 17:51:06 -0400

    Getting started website first draft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,6 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BsDropdownModule.forRoot(),
     BrowserModule
   ],
   providers: [],
